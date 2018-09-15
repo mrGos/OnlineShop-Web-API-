b70944d717ce13d747cefd8582e08ee827d69d10
mrGos <hoangkhang0235@gmail.com>
2018-09-15 11:04:01 +0700

    add entity framework codefirst for data

---
+++ TeduShop.Model/Models/ProductTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProductTags")]
+    public class ProductTag
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        public String TagID { get; set; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Products { get; set; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { get; set; }
+    }
+}
