ab3303c78ce098e5cdb8c97ee3e8895cab1d6435
mrGos <hoangkhang0235@gmail.com>
2018-09-24 12:12:10 +0700

    ok

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="../scripts/angular.js" />
+
+(function () {
+    angular.module('shop', ['shop.products', 'shop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
