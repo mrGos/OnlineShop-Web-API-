fb8d781dc65e40baa7bed52c9c33ed48bec68f62
mrGos <hoangkhang0235@gmail.com>
2018-09-26 13:45:24 +0700

    add search danh sach san pham

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -5,12 +5,31 @@
 
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
-
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
+        $scope.keyword = '';
 
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getallparents', null, function (result) {
-                $scope.productCategories = result.data;
+        $scope.search = search;
+
+        function search() {
+            getProductCategories();
+        }
+
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    keyword: $scope.keyword,
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('load product category failed.')
             });
