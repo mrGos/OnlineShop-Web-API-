83f21a3b7786706477c84b55a256ebd5199ae1e6
mrGos <hoangkhang0235@gmail.com>
2018-09-29 16:43:56 +0700

    add ckeditor

--- TeduShop.Web/app/components/products/productAddController.js
+++ TeduShop.Web/app/components/products/productAddController.js
@@ -1,7 +1,44 @@
 ﻿(function (app) {
     app.controller('productAddController', productAddController);
 
-    function productAddController() {
+    productAddController.$inject = ['apiService', '$scope', 'notificationService', '$state', 'commonService'];
 
+    function productAddController(apiService, $scope, notificationService, $state, commonService) {
+        $scope.product = {
+            CreatedDate: new Date(),
+            Status: true
+        }
+
+
+        $scope.GetSeoTitle = GetSeoTitle;
+
+        function GetSeoTitle() {
+            $scope.product.Alias = commonService.getSeoTitle($scope.product.Name);
+        }
+
+        $scope.AddProduct = AddProduct;
+
+        function AddProduct() {
+            apiService.post('api/product/create', $scope.product, function (result) {
+                notificationService.displaySuccess(result.data.Name + ' đã được thêm mới.');
+                $state.go('products');
+            }, function (error) {
+                notificationService.displayError(result.data.Name + ' thêm mới không thành công.');
+            });
+        }
+
+        function loadProductCategory() {
+            apiService.get('/api/productcategory/getallparents', null, function (result) {
+                $scope.productCategories = result.data;
+            }, function (error) {
+                notificationService.displayError('Lấy loại sản phẩm thất bại.');
+            });
+        }
+
+        loadProductCategory();
     }
+
 })(angular.module('shop.products'));
+
+
+
