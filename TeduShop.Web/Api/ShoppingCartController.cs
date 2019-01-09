7fe18c34678267867232fc9f68f4eb0c58a0714e
mrGos <hoangkhang0235@gmail.com>
2019-01-09 20:05:51 +0700

    add api mobile authentication and orders

---
+++ TeduShop.Web/Api/ShoppingCartController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+using TeduShop.Web.Infrastructure.Core;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Api
+{
+    [RoutePrefix("api/hoppingCart")]
+    public class ShoppingCartController : ApiControllerBase
+    {
+        #region Initialize
+        private IOrderService _orderService;
+        IProductService _productService;
+
+        public ShoppingCartController(IErrorService errorService, IProductService productService, IOrderService orderService)
+            : base(errorService)
+        {
+            this._productService = productService;
+            this._orderService = orderService;
+        }
+
+        #endregion
+
+        [Route("getallorders")]
+        [HttpGet]
+        public HttpResponseMessage GetAllOrders(HttpRequestMessage request, int page, int pageSize = 20)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                int totalRow = 0;
+                var model = _orderService.GetAll();
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<Order>, IEnumerable<OrderViewModel>>(query.AsEnumerable());
+
+                var paginationSet = new PaginationSet<OrderViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
+                return response;
+            });
+        }
+
+    }
+}
