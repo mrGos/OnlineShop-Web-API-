d5e74b659c0dd8fdbed94588a8576166a2dd116a
mrGos <hoangkhang0235@gmail.com>
2019-01-10 10:51:26 +0700

    all change update

--- TeduShop.Web/Api/ShoppingCartController.cs
+++ TeduShop.Web/Api/ShoppingCartController.cs
@@ -5,14 +5,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using TeduShop.Model.Models;
 using TeduShop.Service;
 using TeduShop.Web.Infrastructure.Core;
+using TeduShop.Web.Infrastructure.Extensions;
 using TeduShop.Web.Models;
 
 namespace TeduShop.Web.Api
 {
-    [RoutePrefix("api/hoppingCart")]
+    [RoutePrefix("api/shoppingCart")]
     public class ShoppingCartController : ApiControllerBase
     {
         #region Initialize
@@ -50,6 +52,59 @@ namespace TeduShop.Web.Api
                     TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
                 };
                 var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
+                return response;
+            });
+        }
+
+        [Route("createcart")]
+        [HttpPost]
+        public HttpResponseMessage CreateShoppingCart(HttpRequestMessage request, string orderViewModel, string listcart)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                //var order = new JavaScriptSerializer().Deserialize<OrderViewModel>(orderViewModel);
+
+                //var orderNew = new Order();
+
+                //orderNew.UpdateOrder(order);
+
+                //var cart = new JavaScriptSerializer().Deserialize<List<ShoppingCartViewModel>>(listcart);
+                //List<OrderDetail> orderDetails = new List<OrderDetail>();
+                //bool isEnough = true;
+                //foreach (var item in cart)
+                //{
+                //    var detail = new OrderDetail();
+                //    detail.ProductID = item.ProductId;
+                //    detail.Quantity = item.Quantity;
+                //    detail.Price = item.Product.Price;
+                //    orderDetails.Add(detail);
+
+                //    isEnough = _productService.SellProduct(item.ProductId, item.Quantity);
+                //    break;
+                //}
+                //if (isEnough)
+                //{
+                //    var orderReturn = _orderService.Create(ref orderNew, orderDetails);
+                //    _productService.Save();
+                //    response = request.CreateResponse(HttpStatusCode.Created, Json(new
+                //    {
+                //        status = true
+                //    }));
+
+                //}
+                //else
+                //{
+                //    response = request.CreateResponse(HttpStatusCode.Created, Json(new
+                //    {
+                //        status = false
+                //    }));
+                //}
+                response = request.CreateResponse(HttpStatusCode.Created, new { status = true });
+
+                //var responseData = Mapper.Map<Product, ProductViewModel>(newProduct);
+                //response = request.CreateResponse(HttpStatusCode.Created, responseData);                
+
                 return response;
             });
         }
